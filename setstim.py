96450e2771e3ade14e7f88636e74efddadf8c10a
LoraC <yinguichen@gmail.com>
2017-06-10 23:50:46 +0800

    貓橘毛

--- setstim.py
+++ setstim.py
@@ -4,6 +4,7 @@ col_b = []#col_b -> down 4 squares
 ups = []#top 4 positions
 pos = []#all position
 downs = []#down 4 position
+colors =[]
 import csv
 import  ast
 with open('vwm2.csv') as csvfile:
@@ -15,15 +16,18 @@ with open('vwm2.csv') as csvfile:
         pos.append(row['pos'])
         ups.append(row['ups'])
         downs.append(row['downs'])
-for i in color:
-    color = ast.literal_eval(i)
-for i in col_b:
-    col_b = ast.literal_eval(i)
-for i in pos:
-    pos = ast.literal_eval(i)
-for i in col_a:
-    col_a = ast.literal_eval(i)
-for i in ups:
-    ups = ast.literal_eval(i)
-for i in downs:
-    downs = ast.literal_eval(i)
+    #print  repr(color)
+    for i in color:
+        color = ast.literal_eval(i)
+        #print  repr(color)
+    for i in col_b:
+        col_b = ast.literal_eval(i)
+    for i in pos:
+        pos = ast.literal_eval(i)
+    for i in col_a:
+        col_a = ast.literal_eval(i)
+    for i in ups:
+        ups = ast.literal_eval(i)
+    for i in downs:
+        downs = ast.literal_eval(i)
+      # print  downs
