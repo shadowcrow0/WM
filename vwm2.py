49fbb513e5c1c298966f44235482d702dc08a9e2
LoraC <yinguichen@gmail.com>
2017-07-10 15:18:56 +0800

    Update vwm2.py

--- vwm2.py
+++ vwm2.py
@@ -2,7 +2,7 @@ from random import sample, shuffle, randint
 from psychopy import core, event, gui, visual, data, info
 from itertools import chain
 FEEDBACK = []
-expInfo = {'ID': '', 'age': '', 'gender': ['Male', 'Female'], 'block': ''}
+expInfo = {'ID': '', 'age': '', 'gender': ['Male', 'Female'], 'block': [1,2,3,4,5,6,'p']}
 gui.DlgFromDict(dictionary=expInfo, title='VWM Task-2', order=['ID', 'age','gender','block'])
 COLORS = [(0, 128, 128),(139, 69, 19),(255, 255, 0),(255, 140, 0),(0, 0, 128),(255, 182, 193),(222, 184, 135),(0, 0, 255),(0, 102, 102),(128, 0, 128),(107, 142, 35)]
 WIN = visual.Window((1024, 768), monitor='testMonitor',color="grey", units="pix", fullscr=True)
@@ -13,17 +13,17 @@ instr = visual.TextStim(win = WIN,text='Remember position and color,\nif color b
 practice = visual.TextStim(win = WIN,text='This is the practice block.\n''Make judgment if color appear belong same frame.Press "Left",/n color appears in different frame, please press "Right".''Now press the "Space" key to start practice block.', pos=(0, -2), height=0.8)
 FIX = visual.TextStim(win = WIN, text='+', height=120, color='white', pos=(0, 0))
 ALERT_MSG = visual.TextStim(win = WIN, pos=(0, 4), height=40,
-                            text='Get Ready for VWM task. Remember color and position, \nPress "Space" to start.',
+                            text='Get Ready for VWM task. Remember color and shape of frame, \nPress "Space" to start.',
                             color='white')
 BLOCK_MSG = visual.TextStim(win = WIN, pos=(0, 4), height=40,
-                            text='Finish 36 trials, take  a rest, if you are ready, \nPress "Space" to start.',
+                            text='Finish 40 trials, take  a rest, if you are ready, \nPress "Space" to start.',
                             color='white')
-class Element(object):
+class Element(object): #initailize the color and position we need in the experiment trials
     def __init__(self, positionz, ups, downs, col_new, color, col_b, col_a, context):
         self.positionz = positionz
         self.ups = ups
         self.downs = downs
-        self.col_new = col_new
+        self.col_new = col_new #eliminate color already have been selected
         self.color = color
         self.col_b = col_b
         self.col_a = col_a
@@ -31,7 +31,7 @@ class Element(object):
 
     def drawContent(self):
         squ_ups = []
-        for i in range(len(self.ups)):
+        for i in range(len(self.ups)): #draw separately by position in the screen
             squ = visual.Rect(WIN, size=[100, 100], lineColor=None)  # , fillColorSpace='rgb255')
             squ_ups.append(squ)
         for idx, squ in enumerate(squ_ups):
@@ -47,7 +47,8 @@ class Element(object):
             squ.setFillColor(self.col_b[idx], 'rgb255')
             squ.draw()
 
-    def drawContext(self):
+    def drawContext(self): #draw the context for later selecting
+        # positions of context are determined by random
         if self.context == 0: #circle up,diamond down
             circle = []
             for j in range(len(self.ups)):
@@ -127,10 +128,10 @@ class Log(Response):
         self.ans = ans
         self.FEEDBACK = FEEDBACK
         self.RT = RT
-    def logData(self):
-        dataFile = open("{}_{}_{}.csv".format(expInfo['ID'] , expInfo['age'] , expInfo['gender']), 'a')
+    def logData(self): #save data 
+        dataFile = open("{}_{}_{}_{}.csv".format(expInfo['block'],expInfo['ID'] , expInfo['age'] , expInfo['gender']), 'a')
         sz = len(self.positionz)
-        dataFile.write('{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}'.format(self.context,sz,self.conds,self.CSI, self.probetype,
+        dataFile.write('{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}'.format(expInfo['block'],self.context,sz,self.conds,self.CSI, self.probetype,
                                                                              self.ans, self.FEEDBACK,self.RT,
                                                                              self.positionz, self.ups, self.downs,self.display_color, self.col_new,self.color,
                                                                              self.col_a, self.col_b)+'\n')
@@ -158,20 +159,14 @@ def decideColor(sz):
     return col_new,colors,col_b,col_a
 
 def choseCondition():
-    condition = range(1,37)
-    conditions = sample(condition, 36)
+    conditions = sample(range(1,41),40)
     return  conditions
-def choseProbetype():
+def choseProbetypeold():
     probetype = sample([0,0,1,2],1)[0]
     return probetype
-def choseCSI(conds):
-    if conds % 3 == 0:
-        CSI = .3
-    elif conds % 3 == 2:
-        CSI = 2
-    else:
-        CSI = 5
-    return CSI
+def choseprobetype(conds):
+    probetype =sample([0,0,1,2],1)[0]
+    return probetype
 
 def decideSZ(conds):
     if conds % 4 ==0:
@@ -185,11 +180,11 @@ def decideSZ(conds):
     return sz
 
 def studyingList(elem):
-    elem.drawContent()
-    elem.drawContext()
+    elem.drawContent() #draw color sqaure
+    elem.drawContext() #draw shape of frame
     WIN.flip()
     core.wait(5)
-def getAns(probetype,ans):
+def getAns(probetype,ans): #get answer
     if probetype == 0 and ans == ['left']:
         FEEDBACK_O.draw()
         FEEDBACK.append(1)
@@ -208,10 +203,10 @@ def getAns(probetype,ans):
     elif probetype == 2 and ans == ['left']:
         FEEDBACK_X.draw()
         FEEDBACK.append(0)
-    elif ans == ['left', 'right'] and ans == ['right', 'left']:
+    elif ans ==  ['left', 'right'] or ans == ['right', 'left']:
         FEEDBACK.append('p')
     return FEEDBACK
-def checkOrder(context,TestingContext,col_a,col_b):
+def checkOrder(context,TestingContext,col_a,col_b): #define color intrusion and color positive
     col_intrusion = None
     col_positive = None
     if context ==0  and TestingContext ==0:
@@ -245,21 +240,21 @@ def getProbeColor(context,col_a,col_b , TestingContext ,col_new,probetype):
     display_color  = display
     return  display_color
 def decideTestingContext():
-    TestingContext = randint(0,1)
-    if TestingContext ==0:
-        TestingContext2 =1
+    TestingContext = randint(0,1) #random decide shape of frame in 1st probe
+    if TestingContext ==0: 
+        TestingContext2 =1 #decide 2nd probe 
     else:
         TestingContext2 =0
     return  TestingContext,TestingContext2
-def Task(elem,conds):
+def probe(elem,conds,CSI):
     TestingContext, TestingContext2 = decideTestingContext()
-    for i in [TestingContext,TestingContext2]:
-        probetype = choseProbetype()
-        CSI = choseCSI(conds)
+    for i in [TestingContext,TestingContext2]: #run pair recognition
+        probetype = choseprobetype(conds)
         conditions = condition(elem.positionz, elem.ups, elem.downs, elem.col_new, elem.color, elem.col_a,
         elem.col_b, elem.context,conds, CSI, probetype, i)
-        cs = conditions
+        cs = conditions #make instance for condition class
         display_color = getProbeColor(cs.context,cs.col_a,cs.col_b , i ,cs.col_new,cs.probetype)
+        #chose color is going to present in the probe by probetype and color
         res = Response(cs.positionz,cs.ups,
                        cs.downs,cs.col_new, cs.color, cs.col_a, cs.col_b,
                        cs.context,cs.conds,cs.CSI, cs.probetype,
@@ -269,47 +264,121 @@ def Task(elem,conds):
         WIN.flip()
         FEEDBACK = getAns(res.probetype,ans)
         WIN.flip()
-        core.wait(.5)
-
-        print  'Acur = {}, Ans= {}, RT={}, probetype = {},CSI = {},' \
-               ' display_color={},order={},conds={},{} = color'\
-            .format(FEEDBACK,
-                    res.ans,res.RT,res.probetype,res.CSI,res.display_color,res.conds,
-                    res.context,res.display_color,res.color)
         logs = Log(res.positionz, res.ups, res.downs, res.col_new, res.color, res.col_a, res.col_b,res.context,
-                 res.conds,res.CSI, res.probetype,res.TestingContext,res.display_color,ans, FEEDBACK,RT)
+         res.conds,res.CSI, res.probetype,res.TestingContext,res.display_color,ans, FEEDBACK,RT)
         Log.logData(logs)
+        core.wait(.3)
+        print  'Acur = {}, Ans= {}, RT={}, probetype = {},CSI = {},' \
+               ' display_color={},order={},conds={},{} = color' \
+            .format(FEEDBACK,
+                    res.ans, res.RT, res.probetype, res.CSI, res.display_color, res.conds,
+                    res.context, res.display_color, res.color)
         FEEDBACK.pop()
 
-def fourtrials():
-    conditions = choseCondition()
-    print  conditions
+
+
+def balance():
+    ''' inti
+       if condition =1 run CSI = (.3,2,5)
+       if condition =2 run CSI = (2,5,.3)
+          condition =3 run CSI = (5,.3,2)
+          condition =4 run CSI = (.3,5,2)
+          condition =5 run CSI = (2,.3,5)
+          condition =6 run CSI = (5,2,.3) 
+    each block contains 80 trials, #run 40 trials seperately'''
+
+    def block(CSI):
+        def trials40(CSI):
+            conditions = choseCondition()
+            for conds in conditions:
+                FIX = visual.TextStim(win=WIN, pos=(0, 0), height=80, text='+', color='white')
+                FIX.draw()
+                WIN.flip()
+                core.wait(.8)
+                sz = decideSZ(conds)
+                context = randint(0, 1)  # decide location of shape of frame
+                pos = decidePos(sz)
+                cols = decideColor(sz)
+                elem = Element(pos[0], pos[1], pos[2], cols[0], cols[1], cols[2], cols[3], context)
+                # make instance for element class
+                studyingList(elem)
+                probe(elem, conds, CSI)
+            print  'finish 40 trials'
+
+        trials40(CSI)
+        BLOCK_MSG.draw()
+        WIN.flip()
+        event.waitKeys(keyList=['space'])
+        phase.draw()
+        WIN.flip()
+        event.waitKeys(keyList=['space'])
+        trials40(CSI)
+        BLOCK_MSG.draw()
+        WIN.flip()
+        event.waitKeys(keyList=['space'])
+        phase.draw()
+        WIN.flip()
+        event.waitKeys(keyList=['space'])
+    if expInfo['block'] ==1:  
+        ALERT_MSG.draw()
+        WIN.flip()
+        event.waitKeys(keyList=['space'])
+        block(.3)
+        block(2)
+        block(5)
+
+    elif expInfo['block'] ==2:
+        ALERT_MSG.draw()
+        WIN.flip()
+        event.waitKeys(keyList=['space'])
+        block(2)
+        block(5)
+        block(.3)
+    elif expInfo['block'] ==3:
+        ALERT_MSG.draw()
+        WIN.flip()
+        event.waitKeys(keyList=['space'])
+        block(5)
+        block(.3)
+        block(2)
+    elif expInfo['block'] ==4:
+        ALERT_MSG.draw()
+        WIN.flip()
+        event.waitKeys(keyList=['space'])
+        block(.3)
+        block(5)
+        block(2)
+
+    elif expInfo['block'] ==5:
+        ALERT_MSG.draw()
+        WIN.flip()
+        event.waitKeys(keyList=['space'])
+        block(2)
+        block(.3)
+        block(5)
+    elif expInfo['block'] ==6:
+        ALERT_MSG.draw()
+        WIN.flip()
+        event.waitKeys(keyList=['space'])
+        block(5)
+        block(2)
+        block(.3)
+def practice(CSI):
+    ALERT_MSG.draw()
+    WIN.flip()
+    event.waitKeys(keyList=['space'])
+    conditions = range(1,9)
     for conds in conditions:
         FIX = visual.TextStim(win=WIN, pos=(0, 0), height=80, text='+', color='white')
         FIX.draw()
         WIN.flip()
         core.wait(.8)
         sz = decideSZ(conds)
-        context =randint(0,1)
+        context = randint(0, 1)
         pos = decidePos(sz)
         cols = decideColor(sz)
         elem = Element(pos[0], pos[1], pos[2], cols[0], cols[1], cols[2], cols[3], context)
-        elem.drawContent()
-        elem.drawContext()
-        WIN.flip()
-        core.wait(5)
-        Task(elem,conds)
-    print  'finish 36 trials'
-def for10Bblock():
-   ALERT_MSG.draw()
-   WIN.flip()
-   event.waitKeys(keyList=['space'])
-   for i in range(10):
-       fourtrials()
-       BLOCK_MSG.draw()
-       WIN.flip()
-       event.waitKeys(keyList=['space'])
-       phase.draw()
-       WIN.flip()
-       event.waitKeys(keyList=['space'])
-for10Bblock()
+        studyingList(elem)
+        probe(elem,conds,CSI)
+practice(2)
+balance()
